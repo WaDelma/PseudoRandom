c580fa778b479117ac9ba626d1bba6f0928c9220
WaDelma <ak.ka@mbnet.fi>
2013-10-03 10:56:26 -0700

    World generation stuff. Not tested much due not having time to create prefabs.

---
+++ Assets/Assets/Editor/WorldGeneratorEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(WorldGenerator))]
+public class WorldGeneratorEditor : Editor {
+	
+	public override void OnInspectorGUI() {
+		WorldGenerator myTarget = (WorldGenerator) target;
+		
+		DrawDefaultInspector();
+		if(GUILayout.Button("nappula")) {
+			myTarget.Generoi();
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
