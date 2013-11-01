db0241aaf77229407e26d4d244098da3f5ead5b5
WaDelma <aopkarja@DCB-20.CSWin.cs.Helsinki.FI>
2013-11-01 22:05:14 +0200

    Generator for single houses working... Some problems with prefabs and only outer shell generated for now (also roof is missing).

--- Assets/WorldGenerator.cs
+++ Assets/WorldGenerator.cs
@@ -3,9 +3,8 @@ using UnityEditor;
 using System.Collections.Generic;
 using System.Collections;
 
-public class WorldGenerator : MonoBehaviour {
-	public float unit = 1.6f;
-	public int unitsInFab = 4;
+public class WorldGenerator : Generator{
+	private static int unitsInFab = 4;
 	
 	public Vector2 roadSpacing = new Vector2(10, 10);
 	public Vector2 dimensions = new Vector2(10, 10);
@@ -16,47 +15,24 @@ public class WorldGenerator : MonoBehaviour {
 	public GameObject roadFork;
 	public GameObject roadCross;
 	
-	public void Init() {
-	}
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	public void Delete(){
-		while(transform.childCount > 0){
-			DestroyImmediate(transform.GetChild(0).gameObject);
-		}
-	}
-	
-	public void Create(GameObject obj, Vector3 pos, Quaternion nion){
-		((GameObject)Instantiate(obj, pos, nion)).transform.parent = this.transform;
-	}
-	
-	public void Generoi() {
-		Init();
+	public override void Generate() {
 		float maxX = dimensions.x * roadSpacing.x;
-		float maxY = dimensions.y * roadSpacing.y;
+		float maxZ = dimensions.y * roadSpacing.y;
 		for(int x = 0; x <= maxX; x++){
-			for(int y = 0; y <= maxY; y++){
-				Vector3 pos = position + new Vector3(x * unitsInFab * unit, 0, y * unitsInFab * unit);
+			for(int z = 0; z <= maxZ; z++){
+				Vector3 pos = position + new Vector3(x * unitsInFab, 0, z * unitsInFab);
+				pos.Scale(new Vector3(units.x, units.y, units.x));
 				
-				if(x % maxX == 0 && y % maxY == 0){
+				if(x % maxX == 0 && z % maxZ == 0){
 					Quaternion turn;
 					if(x == 0){
-						if(y == 0){
+						if(z == 0){
 							turn = Quaternion.Euler(0, 180, 0);
 						}else{
 							turn = Quaternion.Euler(0, -90, 0);
 						}
 					}else{
-						if(y == 0){
+						if(z == 0){
 							turn = Quaternion.Euler(0, 90, 0);
 						}else{
 							turn = Quaternion.identity;
@@ -68,7 +44,7 @@ public class WorldGenerator : MonoBehaviour {
 				
 				
 				if(x % maxX == 0){
-					if(y % roadSpacing.y == 0){
+					if(z % roadSpacing.y == 0){
 						Quaternion turn = Quaternion.identity;
 						if(x != 0){
 							turn *= Quaternion.Euler(0, 180, 0);
@@ -77,10 +53,10 @@ public class WorldGenerator : MonoBehaviour {
 						continue;
 					}
 				}
-				if(y % maxY == 0){
+				if(z % maxZ == 0){
 					if(x % roadSpacing.x == 0){
 						Quaternion turn = Quaternion.Euler(0, -90, 0);
-						if(y != 0){
+						if(z != 0){
 							turn *= Quaternion.Euler(0, 180, 0);
 						}
 						Create(roadFork, pos, turn);
@@ -90,14 +66,14 @@ public class WorldGenerator : MonoBehaviour {
 				
 				
 				if(x % roadSpacing.x == 0){
-					if(y % roadSpacing.y == 0){
+					if(z % roadSpacing.y == 0){
 						Create(roadCross, pos, Quaternion.identity);
 						continue;
 					}
 					Create(road, pos, Quaternion.identity);
 					continue;
 				}
-				if(y % roadSpacing.y == 0){
+				if(z % roadSpacing.y == 0){
 					Create(road, pos, Quaternion.Euler(0, 90, 0));
 					continue;
 				}
